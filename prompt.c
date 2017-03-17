9ac2965ce88bd046a87a7a95f1c0bf554691f36e
Ruy <ruyvalle@yahoo.ca>
2017-03-17 11:18:39 -0400

    Done chapter 8, but not the "Bonus marks".

--- prompt.c
+++ prompt.c
@@ -25,9 +25,61 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
-long eval(mpc_ast_t*);
-long eval_op(long, char*, long);
-int count_leaves(mpc_ast_t*); 
+// lvals represent the result of evaluating a lisp
+// expression
+enum {LVAL_NUM, LVAL_ERR};
+enum {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
+typedef struct {
+	int type;
+	long num;
+	int err;
+} lval;
+
+lval lval_num(long x) {
+	lval v;
+	v.type = LVAL_NUM;
+	v.num = x;
+	return v;
+}
+
+lval lval_err(int x) {
+	lval v;
+	v.type = LVAL_ERR;
+	v.err = x;
+	return v;
+}
+
+void lval_print(lval v) {
+	switch (v.type) {
+		case LVAL_NUM:
+			printf("%li", v.num);
+			break;
+		case LVAL_ERR:
+			switch (v.err) {
+				case LERR_DIV_ZERO:
+					printf("Error: Division by zero.");
+					break;
+				case LERR_BAD_OP:
+					printf("Error: Invalid operator.");
+					break;
+				case LERR_BAD_NUM:
+					printf("Error: Invalid number.");
+					break;
+			}
+		break;
+	}
+}
+
+void lval_println(lval v) {
+	lval_print(v);
+	putchar('\n');
+}
+
+lval eval(mpc_ast_t*);
+lval eval_op(lval, char*, lval);
+int count_leaves(mpc_ast_t*);
+int count_branches(mpc_ast_t*);
+int most_children(mpc_ast_t*);
 
 int main(int argc, char** argv) {
     
@@ -39,7 +91,8 @@ int main(int argc, char** argv) {
     
     mpca_lang(MPCA_LANG_DEFAULT,
         "\
-          operator: '+' | '/' | '*' | '-' | '%'; \
+          operator: '+' | '/' | '*' | '-' | '%' | '^' |\
+		  	\"min\" | \"max\" ; \
           number: /-?[0-9]+[.]?[0-9]*/; \
           expr: <number> | '(' <operator> <expr>+ ')'; \
           lispr: /^/ <operator> <expr>+ /$/; \
@@ -57,8 +110,8 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if(mpc_parse("<stdin>", input, Lispr, &r)) {
             // On success, evaluate the input
-            printf("%li\n", eval(r.output));
-            mpc_ast_print(r.output);
+			lval result = eval(r.output);
+			lval_println(result);
             mpc_ast_delete(r.output);
         }
         else {
@@ -74,15 +127,17 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-long eval(mpc_ast_t* t) {
+lval eval(mpc_ast_t* t) {
     // if it's a number, return it directly
     if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
+		errno = 0;
+		long x = strtol(t->contents, NULL, 10);
+        return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
     }
     
     // the operator is always second child
     char* op = t->children[1]->contents;
-    long x = eval(t->children[2]);
+    lval x = eval(t->children[2]);
     
     int i = 3;
     while (strstr(t->children[i]->tag, "expr")) {
@@ -93,17 +148,33 @@ long eval(mpc_ast_t* t) {
     return x;
 }
 
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) return x + y;
-    if (strcmp(op, "-") == 0) return x - y;
-    if (strcmp(op, "*") == 0) return x * y;
-    if (strcmp(op, "/") == 0) return x / y;
-    if (strcmp(op, "%") == 0) return x % y;
-    return 0;
+lval eval_op(lval x, char* op, lval y) {
+	if (x.type == LVAL_ERR) return x;
+	if (y.type == LVAL_ERR) return y;
+	
+    if (strcmp(op, "+") == 0) return lval_num(x.num + y.num);
+    if (strcmp(op, "-") == 0) return lval_num(x.num - y.num);
+    if (strcmp(op, "*") == 0) return lval_num(x.num * y.num);
+    if (strcmp(op, "/") == 0) {
+		return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+	}
+    if (strcmp(op, "%") == 0) {
+		return y.num == 0 ? lval_err(LERR_DIV_ZERO) : lval_num(x.num % y.num);
+	}
+	if (strcmp(op, "^") == 0) {
+		long result = 1;
+		for (long i = 0; i < y.num; i++) {
+			result *= x.num;
+		}
+		return lval_num(result);;
+	}
+	if (strcmp(op, "min") == 0) return x.num > y.num ? y : x;
+	if (strcmp(op, "max") == 0) return x.num > y.num ? x : y;
+	
+	return lval_err(LERR_BAD_OP);
 }
 
 int count_leaves(mpc_ast_t* t) {
-    mpc_ast_print(t);
     int child_num = t->children_num;
     if (child_num == 0) return 1;
     int total = 0;
@@ -111,4 +182,26 @@ int count_leaves(mpc_ast_t* t) {
         total = total + count_leaves(t->children[i]);
     }
     return total;
+}
+
+int count_branches(mpc_ast_t* t) {
+    int child_num = t->children_num;
+    if (child_num == 0) return 0;
+    int total = 1;
+    for (int i = 0; i < child_num; i++) {
+        total = total + count_branches(t->children[i]);
+    }
+    return total;
+}
+
+int most_children(mpc_ast_t* t) {
+	int child_num = t->children_num;
+    int most = child_num;
+	for (int i = 0; i < child_num; i++) {
+		int children = most_children(t->children[i]);
+		if (children > most) {
+			most = children;
+		}
+	}
+	return most;
 }
