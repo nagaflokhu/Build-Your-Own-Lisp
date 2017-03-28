f9f1071974193d200ed822fffc4bf0eec34b7951
Ruy Valle <ruyvalle@yahoo.ca>
2017-03-28 02:12:51 -0400

    Doing chapter 9. Trying to incorporate the change in what lvals can store and the change from lval to lval*.

--- prompt.c
+++ prompt.c
@@ -27,49 +27,61 @@ void add_history(char* unused) {}
 
 // lvals represent the result of evaluating a lisp
 // expression
-enum {LVAL_INT, LVAL_DBL, LVAL_ERR};
-enum {LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM};
-// enum to figure out whether we are dealing with operations
-// with two integers or two doubles
-enum {FIRST_INT, FIRST_DBL, TWO_INTS, TWO_DBLS};
+enum {LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR};
+enum {LONG, DOUBLE};
+
+// Since lvals can either hold longs or doubles, I'm creating
+// a union type, Num, to holdthe values, which should simplify
+// later code.
 typedef struct {
-	int type;
-	double dbl;
-    long num;
-	int err;
+    union {
+        long l;
+        double d;
+    };
+    int type;
+} Num;
+
+typedef struct lval {
+    int type;
+	Num num;
+    
+    // the error and symbols will be stored as strings
+    char* err;
+    char* sym;
+    
+    // counter and pointer to a list of lval pointers
+    int count;
+    struct lval** cell;
 } lval;
 
-lval lval_int(long x) {
-	lval v;
-	v.type = LVAL_INT;
-	v.num = x;
+lval* lval_num(Num x) {
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_NUM;
+	v->num = x;
 	return v;
 }
 
-lval lval_dbl(double x) {
-    lval v;
-    v.type = LVAL_DBL;
-    v.dbl = x;
-    return v;
-}
-
-lval lval_err(int x) {
-	lval v;
-	v.type = LVAL_ERR;
-	v.err = x;
+lval* lval_err(char* x) {
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_ERR;
+	v->err = x;
 	return v;
 }
 
-void lval_print(lval v) {
-	switch (v.type) {
-		case LVAL_INT:
-			printf("%ld", v.num);
-			break;
-        case LVAL_DBL:
-            printf("%f", v.dbl);
+void lval_print(lval* v) {
+	switch (v->type) {
+		case LVAL_NUM:
+            switch (v->num.type) {
+                case LVAL_LONG:
+                    printf("%ld", v->num.l);
+                    break;
+                case LVAL_DOUBLE:
+                    printf("%f", v->num.d);
+                    break;
+            }
             break;
 		case LVAL_ERR:
-			switch (v.err) {
+			switch (v->err) {
 				case LERR_DIV_ZERO:
 					printf("Error: Division by zero.");
 					break;
@@ -84,13 +96,13 @@ void lval_print(lval v) {
 	}
 }
 
-void lval_println(lval v) {
+void lval_println(lval* v) {
 	lval_print(v);
 	putchar('\n');
 }
 
-lval eval(mpc_ast_t*);
-lval eval_op(lval, char*, lval);
+lval* eval(mpc_ast_t*);
+lval* eval_op(lval, char*, lval);
 int count_leaves(mpc_ast_t*);
 int count_branches(mpc_ast_t*);
 int most_children(mpc_ast_t*);
@@ -98,21 +110,25 @@ int most_children(mpc_ast_t*);
 int main(int argc, char** argv) {
     
     // Create parser
-    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Integer = mpc_new("integer");
     mpc_parser_t* Double = mpc_new("double");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr = mpc_new("sexpr");
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lispr = mpc_new("lispr");
     
     mpca_lang(MPCA_LANG_DEFAULT,
         "\
-          operator: '+' | '/' | '*' | '-' | '%' | '^' |\
+          number: <double> | <integer> \
+          integer: /-?[0-9]+/ \
+          double: /-?[0-9]+[.][0-9]*/ \
+          symbol: '+' | '/' | '*' | '-' | '%' | '^' |\
 		  	\"min\" | \"max\" ; \
-          double: /-?[0-9]+[.][0-9]*/; \
-          integer: /-?[0-9]+/; \
-          expr: <double> | <integer> | '(' <operator> <expr>+ ')'; \
-          lispr: /^/ <operator> <expr>+ /$/; \
-        ", Operator, Double, Integer, Expr, Lispr);
+          sexpr: '(' <expr>* ')'; \
+          expr: <number> | <symbol> | <sexpr>; \
+          lispr: /^/ <expr>* /$/; \
+        ", Number, Integer, Double, Symbol, Sexpr, Expr, Lispr);
     
     puts("Lispr Version 0.0.0.0.1");
     puts("Press ctrl+c to Exit\n");
@@ -126,7 +142,7 @@ int main(int argc, char** argv) {
         mpc_result_t r;
         if(mpc_parse("<stdin>", input, Lispr, &r)) {
             // On success, evaluate the input
-			lval result = eval(r.output);
+			lval* result = eval(r.output);
 			lval_println(result);
             mpc_ast_delete(r.output);
         }
@@ -135,44 +151,52 @@ int main(int argc, char** argv) {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
+        // need to free result?
         free(input);
     }
     
     // Deallocate memory
-    mpc_cleanup(5, Operator, Integer, Double, Expr, Lispr);
+    mpc_cleanup(7, Number, Integer, Double Symbol, Sexpr, Expr, Lispr);
     return 0;
 }
 
-lval eval(mpc_ast_t* t) {
+lval* eval(mpc_ast_t* t) {
     // if it's a number, return it directly
+    Num n;
     if (strstr(t->tag, "integer")) {
 		errno = 0;
-		long x = strtol(t->contents, NULL, 10);
-        return errno != ERANGE ? lval_int(x) : lval_err(LERR_BAD_NUM);
+		n.l = strtol(t->contents, NULL, 10);
+        n.type = LONG;
+        return errno != ERANGE ? lval_num(n) : lval_err(LERR_BAD_NUM);
     }
-    
     if (strstr(t->tag, "double")) {
         errno = 0;
-        double x = strtod(t->contents, NULL);
-        return errno != ERANGE ? lval_dbl(x) : lval_err(LERR_BAD_NUM);
+        n.d = strtod(t->contents, NULL);
+        n.type = DOUBLE;
+        return errno != ERANGE ? lval_num(n) : lval_err(LERR_BAD_NUM);
     }
     
     // the operator is always second child
     char* op = t->children[1]->contents;
-    lval x = eval(t->children[2]);
+    lval* x = eval(&t->children[2]);
     
     int i = 3;
     while (strstr(t->children[i]->tag, "expr")) {
-        x = eval_op(x, op, eval(t->children[i]));
+        x = eval_op(x, op, eval(&t->children[i]));
         i++;
     }
     
     return x;
 }
 
-lval eval_op(lval x, char* op, lval y) {
-	if (x.type == LVAL_ERR) return x;
-	if (y.type == LVAL_ERR) return y;
+lval* eval_op(lval* x, char* op, lval* y) {
+	if (x->type == LVAL_ERR) return x;
+	if (y->type == LVAL_ERR) return y;
+    
+    // If either of them is a double, then both can be converted
+    // to doubles. Otherwise, they are both kept as longs.
+    if (x->num.type == DOUBLE || y->num.type == DOUBLE) {
+        
     
     int type;
     if (x.type == LVAL_INT && y.type == LVAL_DBL)
